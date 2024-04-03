a23e118545c2a6b0fed4acc550ca7cb273eb2333
tejas-gojariya <tejasgojariya036@gmail.com>
2024-04-03 18:28:53 +0530

    Add Profile and delete post API using express

--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import uploadRoute from "./routes/uploadRoute.js";
 import commentRoute from "./routes/commentRoutes.js";
-// import { deleteUserById } from "./controllers/deleteController.js";
+import User from "./models/user.js";
+import Post from "./models/post.js";
+import Profile from "./models/Profile.js";
 
 const app = express();
 dotenv.config();
@@ -22,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(bodyParser.json());
 
-// to serve images inside public folder
+// to server images inside public folder
 app.use(express.static("public"));
 app.use("/images", express.static("images"));
 
@@ -32,7 +34,115 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/upload", uploadRoute);
 app.use("/api/comments", commentRoute);
-// app.use("/api/delete", deleteUserById)
+
+// upload a profile
+app.post("/api/profile", async(req, res) => {
+    console.log("profile data is updated");
+    try {
+        const { bio, handle, username, email, number, other } = req.body;
+
+        const userProfile = new Profile({
+            bio,
+            handle,
+            username,
+            email,
+            number,
+            other,
+        });
+        await userProfile.save();
+
+        res.status(201).json({ message: "User profile uploaded successfully" });
+    } catch (error) {
+        console.error("Error uploading user profile:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+// update User
+app.patch("/api/profile/:id", async(req, res) => {
+    try {
+        const profileId = req.params.id;
+        const { username, handle, bio } = req.body;
+
+        const updatedProfile = await Profile.findByIdAndUpdate(
+            profileId, { username, handle, bio }, { new: true }
+        );
+        if (!updatedProfile) {
+            return res.status(404).send("Profile Not define");
+        }
+        res.status(200).send(updatedProfile);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+    console.log("update Profile Success");
+});
+
+// get All Profiles
+app.get("/api/getall", async(req, res) => {
+    console.log("Get All profile");
+    try {
+        const posts = await Profile.find();
+        res.status(200).send(posts);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+// delete profiles
+app.delete("/api/profile/delete/:id", async(req, res) => {
+    console.log("delete post successfully");
+    try {
+        const postId = req.params.id;
+        const post = await Profile.findByIdAndDelete(postId);
+
+        if (!post) {
+            return res.status(404).send("Post not found");
+        }
+
+        res.status(200).send("Profile deleted successfully");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+// delete User
+app.delete("/api/delete/:id", async(req, res) => {
+    console.log("user delete successfully");
+    try {
+        const id = req.params.id;
+        const data = await User.findByIdAndDelete(id);
+        res.send(`Document with ${data.username} has been "deleted"..`);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// Post Delete By Id
+app.delete("/posts/:id", async(req, res) => {
+    console.log("delete post successfully");
+    try {
+        const postId = req.params.id;
+        const post = await Post.findByIdAndDelete(postId);
+
+        if (!post) {
+            return res.status(404).send("Post not found");
+        }
+
+        res.status(200).send("Post deleted successfully");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+// count All post
+app.get("/api/countAllPost", async(req, res) => {
+    try {
+        const datanumber = await Post.countDocuments();
+        res.send(`We have ${datanumber} users in 'post' collection`);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 // error middleware
 app.use(errorHandler);
